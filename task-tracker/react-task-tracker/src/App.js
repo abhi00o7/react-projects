285e08db8b7bd4630610c9436480b32ae72f68f2
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-17 22:58:08 +0530

    Update App.js

--- task-tracker/react-task-tracker/src/App.js
+++ task-tracker/react-task-tracker/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Hello from JavaScript</h1>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
