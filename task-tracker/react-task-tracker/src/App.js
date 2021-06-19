f2e99762e1f288b3adee50b8af1a1b172fe06f04
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-19 22:53:42 +0530

    Update App.js

--- task-tracker/react-task-tracker/src/App.js
+++ task-tracker/react-task-tracker/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from JavaScript</h1>
+      <hr />
+      <br />
+      <h3>this is how I react</h3>
+      <div>
+        <h4>This is a level 4 Heading</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione reiciendis corrupti, non omnis veritatis enim assumenda doloremque, repellendus rerum vitae nulla esse iste officia! Molestiae possimus rem amet nobis harum.</p>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>345</li>
+        </ul>
+      </div>
+      <div><h1>Hello React!</h1></div>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
