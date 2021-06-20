9e0f618e525175e24c2389a18454529a8e5dae75
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-20 23:45:14 +0530

    created Header component

--- task-tracker/react-task-tracker/src/App.js
+++ task-tracker/react-task-tracker/src/App.js
@@ -1,38 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from JavaScript</h1>
       <hr />
-      <br />
-      <h3>this is how I react</h3>
-      <div>
-        <h4>This is a level 4 Heading</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione reiciendis corrupti, non omnis veritatis enim assumenda doloremque, repellendus rerum vitae nulla esse iste officia! Molestiae possimus rem amet nobis harum.</p>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>345</li>
-        </ul>
-        <p>I've just found out that this works on any device connected to the network so anaaaaaaaaaaaazinnnnggggggggggggggggggggg...</p>
-      </div>
-      <div><h1>Hello React!</h1></div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <Header/>
+ 
     </div>
   );
 }
