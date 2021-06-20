4263582e2dc73982574329470e5c096f222e920c
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-21 01:07:25 +0530

    Update Header.js

--- task-tracker/react-task-tracker/src/components/Header.js
+++ task-tracker/react-task-tracker/src/components/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     return (
         <header className='header'>
             <h1>Task Tracker</h1>
+            <Button color='green' text='Hello' />
         </header>
     )
 }
