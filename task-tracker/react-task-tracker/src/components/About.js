68d12141b0585a919b90e10ec7557a4fb03a4eaf
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-22 01:59:10 +0530

    Update About.js

--- task-tracker/react-task-tracker/src/components/About.js
+++ task-tracker/react-task-tracker/src/components/About.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const About = () => {
   return (
     <div>
-      <h4>Version 1.0.0</h4>
+      <h4>Version 0.1.0</h4>
       <Link to='/'>Go Back</Link>
     </div>
   )
