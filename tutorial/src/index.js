30adabbfea83748182a25fb27da32d8a7619450b
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-29 21:50:59 +0530

    create and delete
    deleted the damaged file and created a tutorial to follow along

---
+++ tutorial/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Greeting() {
+  return (
+    <div>
+      <h4>This is not my first JSX</h4>
+    </div>
+  )
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'))
+
+// export default index
