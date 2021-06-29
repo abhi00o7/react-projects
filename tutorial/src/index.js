52b4487db60bca9a1be7d1c6854bfbe83a45e7fa
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-30 00:25:55 +0530

    basic functional components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// stateless functional component
 function Greeting() {
   return (
     <div>
+      <Person/>
       <h4>This is not my first JSX</h4>
+      <ul>
+        <li>
+          <a href="#main">Sparta</a>
+        </li>
+        <li>
+          <Message/>
+        </li>
+        <li>
+          <Kya/>
+        </li>
+      </ul>
     </div>
   )
 }
+const Person = () =>{ return <h2>Jon Pierce</h2>}  
+const Message = () => <p>Gali gali me shor hai</p>  
+const Kya = () => <p>Pierce bhai chor hai</p>  
 
-ReactDOM.render(<Greeting />, document.getElementById('root'))
+ReactDOM.render(<Greeting /> , document.getElementById('root'))
 
