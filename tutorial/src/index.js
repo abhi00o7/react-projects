2bc1bda805ea7ba7e1827e68a7da2a671fbe45f4
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-30 02:10:22 +0530

    props

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -7,20 +7,17 @@ function BookList() {
   return (
     <section>
       <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-   
-    </section>
+      </section>
   )
 }
 const Book = () =>{
-  return <article>
+  const title = 'The Blue Umbrella'
+  const author = 'Ruskin Bond'
+
+return <article className="book">
     <Image></Image>
-    <Title/>
-    <Author/>
+    <h1>{title}</h1>
+    <h4>{author}</h4>
   </article>
 }  
 
@@ -28,14 +25,6 @@ const Image = () =>{
  return  <img src="https://images-eu.ssl-images-amazon.com/images/I/71JZ0neAP1L._AC_UL200_SR200,200_.jpg" alt="Book Cover" />
 }
 
-const Title = () =>{
-  return <h1>
-  The Blue Umbrella
-</h1>
-}
-const Author = () => {
-  return <h4>Ruskin Bond</h4>
-}
 
 ReactDOM.render(<BookList /> , document.getElementById('root'))
 
