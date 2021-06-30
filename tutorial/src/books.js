325867c83352b8aa3a91395b3d421d5813ef7589
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-30 16:51:09 +0530

    added screenshot

--- tutorial/src/books.js
+++ tutorial/src/books.js
@@ -2,16 +2,16 @@ export const data = [
  {
   id: 1,
   img:
-   'https://m.media-amazon.com/images/I/71FCvwtL8-L._AC_UL320_.jpg',
-  title: 'F*cking History: 111 Lessons You Should Have Learned in School',
-  author: 'The Captain',
+   'https://m.media-amazon.com/images/I/51IPS8ijxlL.jpg',
+  title: 'The Secrets We Keep: A gripping emotional page turner',
+  author: 'Kate Hewitt',
  },
  {
   id: 2,
   img:
    'https://images-na.ssl-images-amazon.com/images/I/51bCiaJcnmL._SX322_BO1,204,203,200_.jpg',
   title: 'The Spy and the Traitor: The Greatest Espionage Story of the Cold War',
-  author: 'Ben Macintyre',
+  author: 'Shannon Olsen',
  },
  {
   id: 3,
