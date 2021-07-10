3cc7d37b37309381ba9457af57098ed481bd54a4
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-07-10 23:49:24 +0530

    added random quotes functionality

--- random-quotes-generator/src/quotesFetcher.js
+++ random-quotes-generator/src/quotesFetcher.js
@@ -5,14 +5,14 @@ function FetchQuotes(){
     const [quote,setQuote] = useState('')
     const [loading,setLoading] = useState('')
     const [author,setAuthor] = useState('')
-
+    let random = Math.floor(Math.random() * 99)
     useEffect(() =>{
         fetch('https://raw.githubusercontent.com/abhi00o7/hindi-quotes-web-crawler/main/hindiQuotes.json')
             .then(res => res.json())
             .then(data =>{ 
                 console.log(data)
-                setQuote(data.quotes[0].quote)
-                setAuthor(data.quotes[0].author)
+                setQuote(data.quotes[random].quote)
+                setAuthor(data.quotes[random].author)
             }
                 )
     },[])
